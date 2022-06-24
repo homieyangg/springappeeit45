5599024e969add106810cdb30a0eb72f29aad037
homieyangg <homieyang@outlook.com>
2022-06-24 11:47:55 +0800

    add file

---
+++ src/main/java/com/eeit45team2/springappeeit45/Springappeeit45Application.java
@@ -0,0 +1,13 @@
+package com.eeit45team2.springappeeit45;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springappeeit45Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springappeeit45Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/eeit45team2/springappeeit45/Springappeeit45ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eeit45team2.springappeeit45;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springappeeit45ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
